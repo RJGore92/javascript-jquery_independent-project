689507e1fa8a6a4d97295a8a4a25305dc445e90c
Robert Gore <DarkburstGX@gmail.com>
2018-12-07 12:44:03 -0800

    First 'final' commit for Epicodus's Intro to Programming Unit Two final project.  This assignment has pending review for Epicodus assignment standards, and revisions may be necessary if standards are not met.  This commit finalizes the initial release of this assignment, completing scripting and modifying html and styling in CSS.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Epicodus Unit Two Final: Coding Track Survey
+
+#### Final Project for Epicodus Unit Two, 12/05/2018 start date, 12/07/2018 first release.
+
+#### By **Robert James "Jimmy" Gore**
+
+## Description
+
+The primary purpose of this site is to establish a basic portfolio under the guidelines established by Epicodus's end-of-unit project for JavaScript and jQuery utilizing various tools and functionality introduced through Epicodus Intro to Programming Unit Two in addition to assets and skills learned from Unit One.  It acts as a comprehensive review of all elements and units taught during the first and second units of Epicodus's tracks.
+
+## Setup/Installation Requirements
+
+* Install Git Bash for Git repository cloning of the project
+* Install Atom for review and edit of Code
+* To access repository for project and review code, first clone repository at the appropriate link, then use Git Bash and/or Atom to open the project folder.
+* GitHub Pages link is RJGore92.github.io/unit-final_coding-survey
+
+## Known Bugs
+
+No known bugs are present in this project.
+
+## Technologies Used
+
+* Git Bash
+* Atom
+* HTML
+* MD
+* CSS
+* Bootstrap
+* JavaScript
+* jQuery
+
+### License
+
+Copyright (c) 2018 **Robert James "Jimmy" Gore**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,176 @@
 var javaCount = 0;
 var cSharpCount = 0;
 var phpCount = 0;
+
+$(document).ready(function() {
+  $("form#pre-start-button").submit(function(event) {
+    event.preventDefault();
+    $("div#intro-headers").slideToggle();
+    $("div#form-a").slideToggle();
+  });
+  $("form#question-one").submit(function(event) {
+    event.preventDefault();
+    var c1Answer = $("input:radio[name=code-one]:checked").val();
+    if (c1Answer === "java") {
+      javaCount++;
+    }
+    else if (c1Answer === "c-sharp") {
+      cSharpCount++;
+    }
+    else {
+      phpCount++;
+    }
+    $("div#form-a").slideToggle();
+    $("div#form-b").slideToggle();
+  });
+  $("form#question-two").submit(function(event) {
+    event.preventDefault();
+    var c2Answer = $("input:radio[name=code-two]:checked").val();
+    if (c2Answer === "java") {
+      javaCount++;
+    }
+    else if (c2Answer === "c-sharp") {
+      cSharpCount++;
+    }
+    else {
+      phpCount++;
+    }
+    $("div#form-b").slideToggle();
+    $("div#form-c").slideToggle();
+  });
+  $("form#question-three").submit(function(event) {
+    event.preventDefault();
+    var c3Answer = $("input:radio[name=code-three]:checked").val();
+    if (c3Answer === "java") {
+      javaCount++;
+    }
+    else if (c3Answer === "c-sharp") {
+      cSharpCount++;
+    }
+    else {
+      phpCount++;
+    }
+    $("div#form-c").slideToggle();
+    $("div#form-d").slideToggle();
+  });
+  $("form#question-four").submit(function(event) {
+    event.preventDefault();
+    var c4Answer = $("input:radio[name=code-four]:checked").val();
+    if (c4Answer === "java") {
+      javaCount++;
+    }
+    else if (c4Answer === "c-sharp") {
+      cSharpCount++;
+    }
+    else {
+      phpCount++;
+    }
+    $("div#form-d").slideToggle();
+    $("div#form-e").slideToggle();
+  });
+  $("form#question-five").submit(function(event) {
+    event.preventDefault();
+    var c5Answer = $("input:radio[name=code-five]:checked").val();
+    if (c5Answer === "java") {
+      javaCount++;
+    }
+    else if (c5Answer === "c-sharp") {
+      cSharpCount++;
+    }
+    else {
+      phpCount++;
+    }
+    console.log(javaCount);
+    console.log(cSharpCount);
+    console.log(phpCount);
+    var colSizeA = javaCount * 2;
+    var colSizeB = cSharpCount * 2;
+    var colSizeC = phpCount * 2;
+
+    if ((javaCount >= cSharpCount) && (javaCount >= phpCount))
+    {
+      $("div#result-bar").append(
+        "<div class='col-md-"+colSizeA+" offset-md-1 java-background'></div>"
+      );
+      if (cSharpCount >= phpCount) {
+        $("div#result-bar").append(
+          "<div class='col-md-"+colSizeB+" c-sharp-background'></div>" +
+          "<div class='col-md-"+colSizeC+" php-background'></div>"
+        );
+      }
+      else {
+        $("div#result-bar").append(
+          "<div class='col-md-"+colSizeC+" php-background'></div>" +
+          "<div class='col-md-"+colSizeB+" c-sharp-background'></div>"
+        );
+      }
+      if (javaCount == cSharpCount) {
+        $("div#result-bar").after(
+          "<p id='result-para'>Based on your answers, it looks like your best tracks to study next would be Java or C#.</p>"
+        );
+      }
+      else if (javaCount == phpCount) {
+        $("div#result-bar").after(
+          "<p id='result-para'>Based on your answers, it looks like your best tracks to study next would be Java or PHP.</p>"
+        );
+      }
+      else {
+        $("div#result-bar").after(
+          "<p id='result-para'>Based on your answers, it looks like your best track to study next would be Java.</p>"
+        );
+      }
+    }
+    else if ((cSharpCount > javaCount) && (cSharpCount >= phpCount)) {
+      $("div#result-bar").append(
+        "<div class='col-md-"+colSizeB+" offset-md-1 c-sharp-background'></div>"
+      );
+      if (javaCount >= phpCount) {
+        $("div#result-bar").append(
+          "<div class='col-md-"+colSizeA+" java-background'></div>" +
+          "<div class='col-md-"+colSizeC+" php-background'></div>"
+        );
+      }
+      else {
+        $("div#result-bar").append(
+          "<div class='col-md-"+colSizeC+" php-background'></div>" +
+          "<div class='col-md-"+colSizeA+" java-background'></div>"
+        );
+      }
+      if (cSharpCount == phpCount) {
+        $("div#result-bar").after(
+          "<p id='result-para'>Based on your answers, it looks like your best tracks to study next would be C# or PHP.</p>"
+        );
+      }
+      else {
+        $("div#result-bar").after(
+          "<p id='result-para'>Based on your answers, it looks like your best track to study next would be C#.</p>"
+        );
+      }
+    }
+    else {
+      $("div#result-bar").append(
+        "<div class='col-md-"+colSizeC+" offset-md-1 php-background'></div>"
+      );
+      if (javaCount >= cSharpCount) {
+        $("div#result-bar").append(
+          "<div class='col-md-"+colSizeA+" java-background'></div>" +
+          "<div class='col-md-"+colSizeB+" c-sharp-background'></div>"
+        );
+      }
+      else {
+        $("div#result-bar").append(
+          "<div class='col-md-"+colSizeB+" c-sharp-background'></div>" +
+          "<div class='col-md-"+colSizeA+" java-background'></div>"
+        );
+      }
+      $("div#result-bar").after(
+        "<p id='result-para'>Based on your answers, it looks like your best tracks to study next would be PHP.</p>"
+      );
+    }
+    $("p#result-para").after(
+      "<p>This is, of course, just a recommendation for what you should look into next.  This is by no means a requirement for you to look into the track(s) we recommend, and you are free to study any other track you wish and even encouraged to work with as many tracks as you feel comfortable doing during your time here at Epicodus.  Greater flexibility and experience will definitely help you in the long term."
+    )
+    $("div#form-e").slideToggle();
+    $("div#results-div").slideToggle();
+  });
+});
